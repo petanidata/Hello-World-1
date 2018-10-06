c53f79c12b9e9697a0db956f947ecbbf3d7f5501
Naufal Aly Nafis <naufalan1@gmail.com>
2018-10-06 16:36:55 +0700

    Guess Number

---
+++ Guess Number.py
@@ -0,0 +1,23 @@
+import random
+print ("\tGuess My Number Games")
+print ("\nThe range of number is 1 - 10")
+print ("And you have 5 chance to guess it")
+trie = 1
+number = random.randint(1, 10)
+guess = int(input("\nGuess the number : "))
+while guess != number:
+    if guess > number:
+        print ("Lower")
+    else:
+        print ("Higher")
+
+    guess = int(input("Guess the number : "))
+    trie += 1
+    if trie == 5:
+        print ("\nGame Over")
+        print ("The number is ", number)
+        break
+
+if guess == number:
+    print ("\nFinish")
+    print ("You guessed the number")
